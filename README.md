dd4e4a41d7094f541e71cad36a48a907f8fd12f0
yanzhiqing <yanzhiqing05@163.com>
2022-02-22 11:55:16 +0800

    创建项目

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # react-learn-demo
+
+# 创建react项目
+npx create-react-app my-app
+cd my-app
+npm start
+
+## React基础
+## jsx语法语法介绍
+JSX语法：JavaScript+XML语法（HTML）
+解读jsx语法：遇到<>按照HTML语法解析，遇到{}按照JavaScript解析
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 全家桶
+
+// document.getElementById('root')获取要插入的容器
+// jsx语法：<h1>Hellow React</h1>
+
+const react="react";
+ReactDOM.render(
+ <h1>Hellow {react}</h1>,
+  document.getElementById('root')
+);
